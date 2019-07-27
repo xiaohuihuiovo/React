a49a4d35909c668ecdb41608641e20fa176d95b5
xiaohuihuiovo <1666352306@qq.com>
2019-07-27 20:18:08 +0800

    第一次上传

---
+++ src/component/CmtItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function CmtItem(props){
+  return <div>
+    <h1>评论人: {props.user}</h1>
+    <p>评论内容: {props.content}</p>
+  </div>
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import CmtList from '@/comments/CmtList'
+
+
+ReactDOM.render( < div >
+    <
+    CmtItem > < /CmtItem> <
+    /div>,document.querySelector('#app'))
