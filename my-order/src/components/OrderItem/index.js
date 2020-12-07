7db5984c9b1bdb0eaad0aa80f448446b806b1fb2
xiaohuihuiovo <lvpenghui58@163.com>
2020-12-07 11:12:24 +0800

    完善评价功能

--- my-order/src/components/OrderItem/index.js
+++ my-order/src/components/OrderItem/index.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import './OrderItem.css'
 
 class OrderItem extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editing: false,
+      stars: props.data.stars || 0,
+      comment: props.data.comment || '',
+    }
+  }
   render() {
     const { product, shop, price, picture, ifCommented } = this.props.data
     return (
@@ -20,15 +28,99 @@ class OrderItem extends Component {
                   已评价
                 </button>
               ) : (
-                <button className="OrderItem-btn OrderItem-btn--red ">
+                <button
+                  className="OrderItem-btn OrderItem-btn--red"
+                  onClick={this.handleEditArea}
+                >
                   评价
                 </button>
               )}
             </div>
           </div>
         </div>
+        {this.state.editing ? this.renderEditArea() : null}
       </div>
     )
+  }
+
+  renderEditArea() {
+    return (
+      <div className="OrderItem-commentContainer">
+        <textarea
+          onChange={this.handleCommentChange}
+          value={this.state.comment}
+          className="OrderItem-comment"
+        />
+        {this.renderStars()}
+        <button
+          className="OrderItem-btns OrderItem-btn--red"
+          onClick={this.handleSubmit}
+        >
+          提交
+        </button>
+        <button
+          className="OrderItem-btns OrderItem-btn--gray"
+          onClick={this.handleCancel}
+        >
+          取消
+        </button>
+      </div>
+    )
+  }
+  renderStars() {
+    const { stars } = this.state
+    return (
+      <div>
+        {[1, 2, 3, 4, 5].map((item, index) => {
+          const lightClass = stars >= item ? 'OrderItem-star-light' : ''
+          return (
+            <span
+              key={index}
+              className={'OrderItem-star' + lightClass}
+              onClick={this.handleClickStars.bind(this, item)}
+            >
+              ★
+            </span>
+          )
+        })}
+      </div>
+    )
+  }
+  handleEditArea = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+  handleCommentChange = (e) => {
+    this.setState({
+      comment: e.target.value,
+    })
+  }
+  handleClickStars = (stars) => {
+    this.setState({
+      stars: stars,
+    })
+  }
+  handleCancel = () => {
+    this.setState({
+      editing: false,
+      stars: this.props.data.stars || 0,
+      comment: this.props.data.comment || '',
+    })
+  }
+  handleSubmit = () => {
+    const { id } = this.props.data
+    const { comment, stars } = this.state
+    this.setState({
+      editing: false,
+    })
+    if (comment.length <= 0) {
+      alert('请输入评价内容')
+      return
+    }
+    this.props.onSubmit(id, comment, stars)
+    console.log('您输入的内容是：' + comment)
+    console.log('您给的星星是：' + stars + '颗')
   }
 }
 
--- my-order/src/components/OrderList/index.js
+++ my-order/src/components/OrderList/index.js
@@ -3,52 +3,55 @@ import './OrderList.css'
 import OrderItem from '../OrderItem'
 
 class OrderList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+    }
+  }
+  componentDidMount() {
+    fetch('/mock/order.json')
+      .then((res) => {
+        if (res.ok) {
+          res.json().then((data) => {
+            this.setState({
+              data,
+            })
+          })
+        }
+      })
+
+      .catch()
+  }
   render() {
-    const data = [
-      {
-        id: 1,
-        shop: '香蕉',
-        picture:
-          'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3932229109,1905286699&fm=26&gp=0.jpg',
-        product: '2元/500g',
-        price: 9.9,
-        ifCommented: true,
-      },
-      {
-        id: 2,
-        shop: '苹果',
-        picture:
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607084247440&di=d2cce9280ace304b051838cb50851737&imgtype=0&src=http%3A%2F%2Ftu.ossfiles.cn%3A9186%2Fgroup1%2FM00%2F04%2F25%2FrBgIBlw_1G3gVLr0AACNaTcO5Ew776.jpg',
-        product: '10元/500g',
-        price: 20,
-        ifCommented: false,
-      },
-      {
-        id: 3,
-        shop: '榴莲',
-        picture:
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607084369242&di=b583341adc16476a5d3f1b87fda9a1cf&imgtype=0&src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20170317%2Ff376eea35a9541bb88ebfb62ba019702_th.jpg',
-        product: '100元/500g',
-        price: 99,
-        ifCommented: false,
-      },
-      {
-        id: 4,
-        shop: '车厘子',
-        picture:
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607084555425&di=d5747a04a365ae0ec1fe3a3a4cd361f2&imgtype=0&src=http%3A%2F%2Fimg2.99114.com%2Fgroup1%2FM00%2FA7%2FE4%2FwKgGMFjTO_aATqB3AAUilm-7N1I135_600_600.jpg',
-        product: '200元/500g',
-        price: 166,
-        ifCommented: true,
-      },
-    ]
     return (
       <div>
-        {data.map((item) => {
-          return <OrderItem key={item.id} data={item} />
+        {this.state.data.map((item) => {
+          return (
+            <OrderItem
+              key={item.id}
+              data={item}
+              onSubmit={this.handleSubmitComment}
+            />
+          )
         })}
       </div>
     )
+  }
+  handleSubmitComment = (id, comment, stars) => {
+    const newData = this.state.data.map((item) => {
+      return item.id == id
+        ? {
+            ...item,
+            comment,
+            stars,
+            ifCommented: true,
+          }
+        : item
+    })
+    this.setState({
+      data: newData,
+    })
   }
 }
 
